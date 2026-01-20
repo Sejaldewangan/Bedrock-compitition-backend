fe768d4179e84e40c4c0db0a84c29a370b97dd04
Sejaldewangan <shubham.dewangan.3440@gmail.com>
2026-01-20 16:48:30 +0530

    depolyment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 // Enable CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173' ,'https://bedrock-compitition-frontend.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
